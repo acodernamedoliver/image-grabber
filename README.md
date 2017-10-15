41a7c8d10c0ee8531a80917e43fdb14fa4acfe1e
Oliver Ayris <19904979+oliverayris@users.noreply.github.com>
2017-10-15 13:45:40 +0300

    Synchronising repository.

---
+++ README.md
@@ -0,0 +1,2 @@
+# image-grabber
+A python script for downloading images from popular websites.
---
+++ script.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+#
+# image-grabber
+#
+# A python script for downloading images from popular websites.
